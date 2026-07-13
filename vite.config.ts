64799657fedb4617060d8323938afc0ad19f847b
yossfelhakim <youremail@gmail.com>
2026-07-13 23:45:57 +0200

    Fix GitHub Pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/arkan-transport/",
+
   plugins: [react()],
+
   server: {
     port: 3000,
     host: "0.0.0.0",
     hmr: process.env.DISABLE_HMR !== "true",
     watch: process.env.DISABLE_HMR === "true" ? null : {},
   },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
